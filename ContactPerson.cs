0ab44ecb7b26e9b01d758d6007342368de7d02f3
Bahulya <sarvalabahulya456@gmail.com>
2021-11-15 21:05:59 +0530

    calculateAddressBook

---
+++ ContactPerson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class ContactPerson
+    {
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public string Adress { get; internal set; }
+        public string City { get; internal set; }
+        public string State { get; internal set; }
+        public int ZipCode { get; internal set; }
+        public double PhoneNumber { get; internal set; }
+        public string Email { get; internal set; }
+
+        internal void displaycontact()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
